a86aca62bfadba67837a1e4caf98e1bdbb7e0979
BCOM\nduong <nam-duong.duong@b-com.com>
2022-02-11 15:02:25 +0100

    update max receive and send message size

--- SolARService_Relocalization/start_relocalization_service_debug.sh
+++ SolARService_Relocalization/start_relocalization_service_debug.sh
@@ -8,9 +8,9 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/linuxbrew/.linuxbrew/lib:.
 ## Set application gRPC server url
 export XPCF_GRPC_SERVER_URL=0.0.0.0:50052
 ## Set application gRPC max receive message size (-1 for max value)
-export XPCF_GRPC_MAX_RECV_MSG_SIZE=7000000
+export XPCF_GRPC_MAX_RECV_MSG_SIZE=-1
 ## Set application gRPC max send message size (-1 for max value)
-export XPCF_GRPC_MAX_SEND_MSG_SIZE=2000000
+export XPCF_GRPC_MAX_SEND_MSG_SIZE=-1
 
 ## Set application log level
 ## Log level expected: DEBUG, CRITICAL, ERROR, INFO, TRACE, WARNING

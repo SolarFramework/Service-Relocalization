9853fd2f32e476a08f7960aea6ecaa0ee94a866e
yzhou <Yitian.Zhou@b-com.com>
2023-01-24 11:50:32 +0100

    Test frondend on captured sequence with 2 gt poses

--- installData.sh
+++ installData.sh
@@ -3,6 +3,7 @@ curl https://github.com/SolarFramework/SolARModuleFBOW/releases/download/fbowVoc
 mkdir -p data/fbow_voc
 unzip -o fbow_voc.zip -d ./data/fbow_voc
 rm fbow_voc.zip
+curl https://repository.solarframework.org/generic/FbowVoc/popsift_uint8.fbow -L -o ./data/fbow_voc/popsift_uint8.fbow
 
 # Download TUM camera calibration
 curl https://repository.solarframework.org/generic/captures/singleRGB/TUM/tum_camera_calibration.json -L -o data/tum_camera_calibration.json
@@ -23,6 +24,10 @@ rm loopDesktopA.zip
 curl https://repository.solarframework.org/generic/captures/hololens/bcomLab/loopDesktopB.zip -L -o loopDesktopB.zip
 unzip -o loopDesktopB.zip -d ./data/data_hololens
 rm loopDesktopB.zip
+
+curl https://repository.solarframework.org/generic/captures/hololens/bcomLab/gtposeFidDesktopA.zip -L -o gtposeFidDesktopA.zip
+unzip -o gtposeFidDesktopA.zip -d ./data/data_hololens
+rm gtposeFidDesktopA.zip
 
 # Download calibration file
 echo Download calibration file

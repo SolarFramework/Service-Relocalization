661d803ea14a56cfd67d22696c2686c858032b08
Christophe Cutullic <christophe.cutullic@b-com.com>
2023-03-17 17:30:49 +0100

    Fix a display problem in OpenGL window

--- SolARService_MappingAndRelocFrontend/tests/SolARServiceTest_MappingAndRelocFrontend_Multi_Viewer/SolARServiceTest_MappingAndRelocFrontend_Multi_Viewer.cpp
+++ SolARService_MappingAndRelocFrontend/tests/SolARServiceTest_MappingAndRelocFrontend_Multi_Viewer/SolARServiceTest_MappingAndRelocFrontend_Multi_Viewer.cpp
@@ -186,10 +186,11 @@ int main(int argc, char* argv[])
 
                 LOG_INFO("==> Display current global map: press ESC on the map display window to end test");
 
-                datastructure::Transform3Df poseNotUsed, transform3D;
+                datastructure::Transform3Df poseNotUsed = Transform3Df::Identity();
                 std::vector<datastructure::Transform3Df> poseReloc = {};
                 std::vector<datastructure::Transform3Df> poseNoReloc = {};
                 api::pipeline::TransformStatus transform3DStatus;
+                datastructure::Transform3Df transform3D;
                 float_t confidence;
 
                 while (true)
